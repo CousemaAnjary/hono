68a1fa3876d284cd99ca45a1f46f0e2414f9ea6c
CousemaAnjary <anjaryabdillah@gmail.com>
2025-06-17 01:03:30 +0300

    feat: ajout de bcrypt pour le hachage des mots de passe et mise à jour des fonctions d'authentification

--- src/repositories/auth.repository.ts
+++ src/repositories/auth.repository.ts
@@ -1,1 +1,20 @@
-export const findUserByEmail = async (email: string) => {}
+import { eq } from "drizzle-orm"
+import db from "../config/drizzle"
+import { users } from "../models"
+
+
+
+export const findUserByEmail = async (email: string) => {
+  const result = await db.select().from(users).where(eq(users.email, email))
+  return result[0] ?? null
+}
+
+export const createUser = async (name:string, email:string, hashedPassword:string) => {
+  const result = await db.insert(users).values({
+    name,
+    email,
+    password: hashedPassword,
+  }).returning()
+
+  return result[0] 
+}
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,7 +1,21 @@
 import type { z } from "zod"
+import * as bcrypt from "bcrypt"
 import type { registerSchema } from "../validators/auth.validator.js"
+import { createUser, findUserByEmail } from "../repositories/auth.repository"
+
 
 export const registerUser = async (data: z.infer<typeof registerSchema>) => {
   // Destructuration des données validées
   const { name, email, password } = data
+
+  // Vérification si l'utilisateur existe déjà
+  const existingUser = await findUserByEmail(email)
+  if (existingUser) throw new Error("cet email est déjà utilisé")
+
+  // hashage du mot de passe
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  // Création de l'utilisateur
+  const newUser = await createUser(name, email, hashedPassword)
+
 }
