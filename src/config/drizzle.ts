f01fc4edd769b0e4ad3c8f07eb6283b69bc507ca
CousemaAnjary <anjaryabdillah@gmail.com>
2025-06-16 23:03:39 +0300

    feat: mise à jour du chemin d'importation du schéma dans drizzle.ts

--- src/config/drizzle.ts
+++ src/config/drizzle.ts
@@ -1,6 +1,6 @@
 import "dotenv/config"
 import { Pool } from "pg"
-import * as schema from "../database/schema/index.js"
+import * as schema from "../models/index.js"
 import { drizzle } from "drizzle-orm/node-postgres"
 
 
