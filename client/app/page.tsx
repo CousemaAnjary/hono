c0a01f48e62bd6ca9fb5bc19399976ddeb0a5048
CousemaAnjary <anjaryabdillah@gmail.com>
2025-06-18 21:36:48 +0300

    feat: ajout du composant Button avec variantes et styles personnalisés

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,14 +1,14 @@
+import { Button } from "@/src/components/ui/button"
+
 export default function page() {
   // ! STATE (état, données) de l'application
-  
-  
+
   // ! ACTIONS (actions, fonctions) de l'application
-  
-  
+
   // ! AFFICHAGE (affichage, UI) de l'application
   return (
     <>
-      
+      <Button> Hello World </Button>
     </>
   )
 }
