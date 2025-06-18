6c8f0532b6dfa59c9f31a3b8c92a236c94122e2d
CousemaAnjary <anjaryabdillah@gmail.com>
2025-06-18 20:08:59 +0300

    fix: ajout du middleware d'authentification aux routes privées

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,23 @@
 import { Hono } from "hono"
 import authRoutes from "./auth.routes"
+import { authMiddleware } from "middlewares/authMiddleware"
 
 const router = new Hono()
+
+// Routes publiques (sans middleware)
+const publicRoutes = new Hono()
   .route("/auth", authRoutes)
 
+
+// Route protégées (avec middleware)
+const privateRoutes = new Hono()
+privateRoutes.use("*", authMiddleware) // Appliquer le middleware à toutes les routes privées
+  // .route("/posts", postRoutes)
+
+
+
+// Regroupement des routes publiques et privées
+router.route("/", publicRoutes)
+router.route("/", privateRoutes)
 
 export default router
