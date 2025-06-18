f5b223760c9925b5c5628f21286199b86e1373ab
CousemaAnjary <anjaryabdillah@gmail.com>
2025-06-18 23:30:03 +0300

    fix: corriger l'URL de l'API pour la connexion et simplifier la validation des données d'entrée

--- client/src/services/auth.service.ts
+++ client/src/services/auth.service.ts
@@ -2,10 +2,11 @@ import { z } from "zod"
 import { LoginResponse } from "../types/auth"
 import { loginSchema } from "../validators/auth.validator"
 
+const apiUrl = process.env.NEXT_PUBLIC_API_URL as string
 
 
 export const login = async (data: z.infer<typeof loginSchema>):Promise<LoginResponse> => {
-  const res = await fetch("/api/auth/login", {
+  const res = await fetch(`${apiUrl}/auth/login`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -3,33 +3,21 @@ import { loginUser, registerUser } from "../services/auth.service"
 import { loginSchema, registerSchema } from "../validators/auth.validator"
 
 export const register = async (c: Context) => {
+
   // validate des données d'entrée (body)
   const validated = registerSchema.safeParse(await c.req.json())
-  if (!validated.success)
-    return c.json(
-      {
-        success: false,
-        message: "Validation échouée",
-        errors: validated.error.flatten().fieldErrors,
-      },
-      400
-    )
-
+  if (!validated.success)return c.json({success: false, message: "Validation échouée", errors: validated.error.flatten().fieldErrors,},400)
+    
   try {
     const newUser = await registerUser(validated.data)
-    return c.json(
-      { success: true, message: "Utilisateur créé avec succès", newUser },
-      201
-    )
+    return c.json({ success: true, message: "Utilisateur créé avec succès", newUser },201)
+      
   } catch (error) {
     if (error instanceof Error) {
       return c.json({ success: false, message: error.message }, 409)
     }
     // En cas d'erreur inconnue
-    return c.json(
-      { success: false, message: "Une erreur inconnue est survenue" },
-      500
-    )
+    return c.json({ success: false, message: "Une erreur inconnue est survenue" },500) 
   }
 }
 
