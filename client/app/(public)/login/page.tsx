23f109e79a08486dbbf77d63db5f3b1fdaf752ec
CousemaAnjary <anjaryabdillah@gmail.com>
2025-06-18 22:00:30 +0300

    feat: ajout de styles personnalisés au composant Button dans la page

--- client/app/(public)/login/page.tsx
+++ client/app/(public)/login/page.tsx
@@ -1,14 +1,13 @@
-import { Button } from "@/src/components/ui/button"
-
-export default function page() {
+export default function LoginPage() {
   // ! STATE (état, données) de l'application
-
+  
+  
   // ! ACTIONS (actions, fonctions) de l'application
-
+  
+  
   // ! AFFICHAGE (affichage, UI) de l'application
   return (
     <>
-      <Button className="bg-blue-950 font-semibold m-2 rounded-sm"> Hello World </Button>
     </>
   )
 }
