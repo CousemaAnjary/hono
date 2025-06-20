d86a2fad44993ba917426d68d05b7001cd2513a7
CousemaAnjary <anjaryabdillah@gmail.com>
2025-06-20 19:26:50 +0300

    feat: ajouter le composant DashboardPage avec une structure de base

---
+++ client/app/(protected)/dashboard/page.tsx
@@ -0,0 +1,16 @@
+export default function DashboardPage() {
+  // ! STATE (état, données) de l'application
+  
+  
+  // ! ACTIONS (actions, fonctions) de l'application
+  
+  
+  // ! AFFICHAGE (affichage, UI) de l'application
+  return (
+    <>
+      <h1>Dashboard</h1>
+      <p>Welcome to your dashboard!</p>
+      {/* Add more components or content here as needed */}
+    </>
+  )
+}
