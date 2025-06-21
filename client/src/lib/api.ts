fcce5b945721c3df64a5e8743e192aa6e16f7f7c
CousemaAnjary <anjaryabdillah@gmail.com>
2025-06-21 17:25:20 +0300

    refactor: simplifier la vérification de l'URL de l'API dans le fichier api.ts

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,5 +1,4 @@
 export const apiUrl = process.env.NEXT_PUBLIC_API_URL as string
 
-if (!apiUrl) {
-  throw new Error("NEXT_PUBLIC_API_URL is not defined in environment variables")
-}
+if (!apiUrl) throw new Error("NEXT_PUBLIC_API_URL is not defined in environment variables")
+
