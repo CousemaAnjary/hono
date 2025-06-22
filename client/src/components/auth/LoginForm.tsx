2917fd7348607e75c1c4ac7705e422812ae9a988
CousemaAnjary <anjaryabdillah@gmail.com>
2025-06-22 16:08:47 +0300

    fix: commenter l'utilisation du router dans le composant LoginForm

--- client/src/components/auth/LoginForm.tsx
+++ client/src/components/auth/LoginForm.tsx
@@ -23,7 +23,7 @@ import { useRouter } from "next/navigation"
 export default function LoginForm() {
   // ! STATE (état, données) de l'application
 
-  const router = useRouter()
+  // const router = useRouter()
   const form = useForm<z.infer<typeof loginSchema>>({
     resolver: zodResolver(loginSchema),
     defaultValues: {
@@ -37,9 +37,9 @@ export default function LoginForm() {
   const handleLogin = async (data: z.infer<typeof loginSchema>) => {
     try {
       const reponse = await login(data)
-      
+
       // Redirection vers la page d'accueil ou dashboard
-      router.push("/dashboard")
+      // router.push("/dashboard")
       toast.success(reponse.message)
 
     } catch (error) {
