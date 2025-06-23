17aba9d027cf421466b73a0ad0d1532efb47cae0
CousemaAnjary <anjaryabdillah@gmail.com>
2025-06-24 01:24:57 +0300

    fix: réactiver le code de la fonction getCurrentUser dans user.service.ts

--- client/src/services/user.service.ts
+++ client/src/services/user.service.ts
@@ -1,9 +1,9 @@
-// import { User } from "../types/auth"
-// import { authFetch } from "../utils/authFetch"
+import { User } from "../types/auth"
+import { authFetch } from "../utils/authFetch"
 
-// export const getCurrentUser = async (): Promise<User | null> => {
-//   const res = await authFetch("/user/me", {
-//     method: "GET",
-//   })
-//   return await res.json()
-// }
+export const getCurrentUser = async (): Promise<User | null> => {
+  const res = await authFetch("/user/me", {
+    method: "GET",
+  })
+  return await res.json()
+}
