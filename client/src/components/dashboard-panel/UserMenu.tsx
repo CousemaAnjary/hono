6068a920529b42afa05a1e99c84b194ce0730079
CousemaAnjary <anjaryabdillah@gmail.com>
2025-06-24 23:05:32 +0300

    feat: intégrer le composant UserMenu avec des données utilisateur dynamiques et améliorer la structure du menu déroulant

--- client/src/components/dashboard-panel/UserMenu.tsx
+++ client/src/components/dashboard-panel/UserMenu.tsx
@@ -1,6 +1,8 @@
 "use client"
 
-import { BoltIcon, LogOutIcon } from "lucide-react"
+import { useUser } from "@/src/hooks/useUser"
+import { LogOutIcon, Settings, User } from "lucide-react"
+import Link from "next/link"
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar"
 import { Button } from "../ui/button"
 import {
@@ -14,11 +16,18 @@ import {
 } from "../ui/dropdown-menu"
 
 export default function UserMenu() {
-  // ! STATE (état, données) de l'application
+  /**
+   * ! STATE (état, données) de l'application
+   */
+  const { data: user } = useUser()
 
-  // ! ACTIONS (actions, fonctions) de l'application
+  /**
+   * ! COMPORTEMENT (méthodes, fonctions) de l'application
+   */
 
-  // ! AFFICHAGE (affichage, UI) de l'application
+  /**
+   * ! AFFICHAGE (render) de l'application
+   */
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -32,24 +41,32 @@ export default function UserMenu() {
       <DropdownMenuContent className="max-w-64" align="end">
         <DropdownMenuLabel className="flex min-w-0 flex-col">
           <span className="text-foreground truncate text-sm font-medium">
-            Keith Kennedy
+            {user?.name}
           </span>
           <span className="text-muted-foreground truncate text-xs font-normal">
-            k.kennedy@originui.com
+            {user?.email}
           </span>
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
 
         <DropdownMenuGroup>
-          <DropdownMenuItem>
-            <BoltIcon size={16} className="opacity-60" aria-hidden="true" />
-            <span>Option 1</span>
+          <DropdownMenuItem className="hover:cursor-pointer" asChild>
+            <Link href="#" className="flex items-center font-inter">
+              <User className="mr-1 size-4 text-muted-foreground" />
+              Profile
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem className="hover:cursor-pointer" asChild>
+            <Link href="#" className="flex items-center font-inter">
+              <Settings className="mr-1 size-4 text-muted-foreground" />
+              Paramètres
+            </Link>
           </DropdownMenuItem>
         </DropdownMenuGroup>
-        
+
         <DropdownMenuSeparator />
-        <DropdownMenuItem>
-          <LogOutIcon size={16} className="opacity-60" aria-hidden="true" />
+        <DropdownMenuItem className="hover:cursor-pointer">
+          <LogOutIcon size={16} className="mr-1 opacity-60" aria-hidden="true" />
           <span>Logout</span>
         </DropdownMenuItem>
       </DropdownMenuContent>
