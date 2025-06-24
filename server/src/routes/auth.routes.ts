e0b0b142ac386258f878b70f4d0e77ba170fb1e1
CousemaAnjary <anjaryabdillah@gmail.com>
2025-06-24 17:34:12 +0300

    fix: commenter l'appel à la route refreshToken dans auth.routes.ts pour simplifier la gestion des routes

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -5,7 +5,6 @@ import { login, register } from "../controllers/auth.controller"
 const authRoutes = new Hono()
   .post('/register', register)
   .post('/login', login)
-  // .post('/refresh', refreshToken)
 
 
 export default authRoutes
