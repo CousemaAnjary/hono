a7d510acca19b2fcb3ecb2d6b5b988252c79a117
CousemaAnjary <anjaryabdillah@gmail.com>
2025-06-24 20:32:07 +0300

    feat: ajouter le service pour récupérer l'utilisateur courant

---
+++ client/src/services/user.service.ts
@@ -0,0 +1,16 @@
+import { apiUrl } from "../lib/api"
+
+
+export const getCurrentUser = async () => {
+  const res = await fetch(`${apiUrl}/user/me`, {
+    method: "GET",
+    credentials: "include",
+  })
+
+   if (!res.ok) {
+    const errorData = await res.json()
+    throw new Error(errorData.message)
+  }
+  
+  return await res.json()
+}
