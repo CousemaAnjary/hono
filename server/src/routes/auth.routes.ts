2b5271a21c03334e8bd924605359649179fab83b
CousemaAnjary <anjaryabdillah@gmail.com>
2025-06-24 16:15:05 +0300

    fix: ajouter un espace vide dans auth.routes.ts

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono"
 import { login, refreshToken, register } from "../controllers/auth.controller"
 
+
 const authRoutes = new Hono()
   .post('/register', register)
   .post('/login', login)
