d8e05af348b490ac2255432466de58d2f9b1189b
CousemaAnjary <anjaryabdillah@gmail.com>
2025-06-24 17:41:29 +0300

    fix: supprimer la logique de récupération de l'utilisateur pour alléger le composant Dashboard

--- client/app/(protected)/dashboard/page.tsx
+++ client/app/(protected)/dashboard/page.tsx
@@ -1,37 +1,15 @@
-"use client"
-
-import { apiUrl } from "@/src/lib/api"
-import { User } from "@/src/types/auth"
-import { useEffect, useState } from "react"
-
-
 export default function DashboardPage() {
-const [user, setUser] = useState<User | null>(null)
-
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const res =  await fetch(`${apiUrl}/user/me`, {
-          method: "GET",
-          credentials: "include",
-        })
-        if (res.ok) {
-          const u: User = await res.json()
-          setUser(u)
-        } else {
-          setUser(null)
-        }
-      } catch (e) {
-        setUser(null)
-      }
-    }
-
-    fetchUser()
-  }, [])
-
-  if (!user) return <p>Non connecté</p>
-
+  // ! STATE (état, données) de l'application
+  
+  
+  // ! ACTIONS (actions, fonctions) de l'application
+  
+  
+  // ! AFFICHAGE (affichage, UI) de l'application
   return (
-    <p>Connecté en tant que : {user.name}</p>
+    <>
+      <h1>Dashboard</h1>
+      <p>Bienvenue sur votre tableau de bord !</p>
+    </>
   )
 }
