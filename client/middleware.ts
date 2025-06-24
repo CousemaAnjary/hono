6156533766a26f77ad9c157fb8df54766b3248f6
CousemaAnjary <anjaryabdillah@gmail.com>
2025-06-24 22:11:18 +0300

    feat: ajouter la logique de redirection dans le middleware en fonction de l'authentification

--- client/middleware.ts
+++ client/middleware.ts
@@ -1,6 +1,35 @@
-import { NextRequest } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 import { getToken } from "./src/lib/cookie"
 
+const protectedRoutes = ["/dashboard"]
+const publicRoutes = ["/", "/login", "/register"]
+
 export async function middleware(request: NextRequest) {
+  //
+  const path = request.nextUrl.pathname
+  const isProtectedRoute = protectedRoutes.includes(path)
+  const isPublicRoute = publicRoutes.includes(path)
+
+  //
   const token = getToken()
+  const isAuthenticated = !!token
+
+  // Redirection si l n'est pas authentifié
+  if (isProtectedRoute && !isAuthenticated) {
+    return NextResponse.redirect(new URL("/login", request.nextUrl))
+  }
+
+  // Redirection si l'utilisateur est authentifié 
+  if (isPublicRoute && isAuthenticated) {
+    return NextResponse.redirect(new URL("/dashboard", request.nextUrl))
+  }
+
+  // Si aucune condition n'est remplie, on continue la requête
+  return NextResponse.next()
+}
+
+// Configuration du middleware pour matcher les routes
+// On ignore les routes API, les fichiers statiques Next.js et les images PNG
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 }
