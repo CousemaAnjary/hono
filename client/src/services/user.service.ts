9819b3eda380398e9ead8f3fdc0295783134b649
CousemaAnjary <anjaryabdillah@gmail.com>
2025-06-25 20:49:51 +0300

    refactor: remplacer la logique asynchrone de récupération de l'utilisateur par une fonction authFetch

--- client/src/services/user.service.ts
+++ client/src/services/user.service.ts
@@ -1,16 +1,19 @@
-import { apiUrl } from "../lib/api"
+import { User } from "../types/auth"
+import { authFetch } from "../utils/authFetch"
 
-export const getCurrentUser = async () => {
-  const res = await fetch(`${apiUrl}/user/me`, {
-    method: "GET",
-    credentials: "include",
-  })
+// export const getCurrentUser = async () => {
+//   const res = await fetch(`${apiUrl}/user/me`, {
+//     method: "GET",
+//     credentials: "include",
+//   })
 
-   if (!res.ok) {
-    const errorData = await res.json()
-    throw new Error(errorData.message)
-  }
-  
-   const data = await res.json()
-  return data.user 
-}
+//    if (!res.ok) {
+//     const errorData = await res.json()
+//     throw new Error(errorData.message)
+//   }
+
+//    const data = await res.json()
+//   return data.user
+// }
+
+export const getCurrentUser = () => authFetch<User>("/user/me")
