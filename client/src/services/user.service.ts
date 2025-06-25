3c00336d2b2af14c7fc9cca3767edbee7779632f
CousemaAnjary <anjaryabdillah@gmail.com>
2025-06-25 21:03:58 +0300

    fix: réorganiser l'importation de getCurrentUserResponse dans user.service.ts

--- client/src/services/user.service.ts
+++ client/src/services/user.service.ts
@@ -1,5 +1,6 @@
-import { getCurrentUserResponse } from "../types/user"
 import { authFetch } from "../utils/authFetch"
+import { getCurrentUserResponse } from "../types/user"
+
 
 // Récupère l'utilisateur connecté
 export const getCurrentUser = async () => {
