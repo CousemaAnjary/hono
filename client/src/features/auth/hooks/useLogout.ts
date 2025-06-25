986df3da885a2e5d602efb58bb8b027c9afb643d
CousemaAnjary <anjaryabdillah@gmail.com>
2025-06-26 01:00:35 +0300

    fix: réorganiser les imports et ajouter le nettoyage du cache sur déconnexion réussie

--- client/src/features/auth/hooks/useLogout.ts
+++ client/src/features/auth/hooks/useLogout.ts
@@ -1,10 +1,11 @@
-import { logout } from "../services/auth.service"
+import { queryClient } from "@/src/lib/react-query"
 import { useMutation } from "@tanstack/react-query"
+import { logout } from "../services/auth.service"
 
 export const useLogout = () => {
-  
+
   return useMutation({
     mutationFn: logout,
-    
+    onSuccess : () => { queryClient.clear() }
   })
 }
