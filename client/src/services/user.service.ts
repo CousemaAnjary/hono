abe9fdfebdb8aadbb81c87c9ce5a92e5eccfd8f7
CousemaAnjary <anjaryabdillah@gmail.com>
2025-06-25 21:02:51 +0300

    refactor: améliorer la fonction getCurrentUser pour une gestion explicite de la réponse

--- client/src/services/user.service.ts
+++ client/src/services/user.service.ts
@@ -1,5 +1,8 @@
-import { authFetch } from "../utils/authFetch"
 import { getCurrentUserResponse } from "../types/user"
+import { authFetch } from "../utils/authFetch"
 
-// Récupère l'utilisateur connecté 
-export const getCurrentUser = async () => authFetch<getCurrentUserResponse>("/user/me")
+// Récupère l'utilisateur connecté
+export const getCurrentUser = async () => {
+  const res = await authFetch<getCurrentUserResponse>("/user/me")
+  return res.user
+}
