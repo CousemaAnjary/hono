910f0f47445f966d47f062365a20bc6bb0dd3880
CousemaAnjary <anjaryabdillah@gmail.com>
2025-06-25 20:56:58 +0300

    fix: corriger le type de retour de la fonction getCurrentUser pour utiliser getCurrentUserResponse

--- client/src/services/user.service.ts
+++ client/src/services/user.service.ts
@@ -1,5 +1,5 @@
-import { User } from "../types/user"
 import { authFetch } from "../utils/authFetch"
+import { getCurrentUserResponse } from "../types/user"
 
 // Récupère l'utilisateur connecté 
-export const getCurrentUser = async () => authFetch<User>("/user/me")
+export const getCurrentUser = async () => authFetch<getCurrentUserResponse>("/user/me")
