619cee5b0ba44fe8500a90ff243a399bf61612bc
CousemaAnjary <anjaryabdillah@gmail.com>
2025-06-26 18:07:57 +0300

    feat: ajouter une barre de recherche stylisée dans le composant RightMenu

--- client/src/components/dashboard-panel/RightMenu.tsx
+++ client/src/components/dashboard-panel/RightMenu.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { Search } from "lucide-react"
 import UserMenu from "../../features/auth/components/UserMenu"
 import NotificationMenu from "./NotificationMenu"
 
@@ -17,10 +18,24 @@ export default function RightMenu() {
    */
   return (
     <div className="flex items-center gap-4">
+      {/* Barre de recherche stylisée */}
+      <div className="relative hidden md:flex items-center">
+        <button
+          type="button"
+          className="group flex items-center gap-2 rounded-md border px-3 py-1.5 text-sm text-muted-foreground shadow-sm transition hover:bg-muted"
+        >
+          <Search className="h-4 w-4 text-gray-500" />
+          <span className="text-sm text-gray-500 font-spaceGrotesk">Rechercher tout</span>
+          <kbd className="ml-auto rounded bg-gray-100 px-1.5 py-0.5 text-xs text-gray-600 font-spaceGrotesk">
+            CTRL + K
+          </kbd>
+        </button>
+      </div>
+
       <div className="flex items-center gap-2">
-        {/* <InfoMenu /> */}
         <NotificationMenu />
       </div>
+
       <UserMenu />
     </div>
   )
