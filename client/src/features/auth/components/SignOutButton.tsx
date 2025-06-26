5d4283b67ee663edd07d0381bf17827eb3ff461e
CousemaAnjary <anjaryabdillah@gmail.com>
2025-06-26 15:34:00 +0300

    fix: supprimer le type User inutilisé dans user.ts

---
+++ client/src/features/auth/components/SignOutButton.tsx
@@ -0,0 +1,19 @@
+export default function SignOutButton() {
+  /**
+   * ! STATE (état, données) de l'application
+   */
+  
+  
+  /**
+   * ! COMPORTEMENT (méthodes, fonctions) de l'application
+   */
+  
+  
+  /**
+   * ! AFFICHAGE (render) de l'application
+   */
+  return (
+    <>
+    </>
+  )
+}
