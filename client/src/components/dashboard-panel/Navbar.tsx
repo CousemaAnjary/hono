042d9e4d7aebabd1a335133c5d55de9e4dbaa4b9
CousemaAnjary <anjaryabdillah@gmail.com>
2025-06-26 17:19:07 +0300

    feat: add navigation and user interface components
    - Added Radix UI for improved navigation and dropdown menus.
    - Implemented Navbar component with navigation links.
    - Created a new component for notifications with state management.
    - Developed InfoMenu, Logo, NotificationMenu, and UserMenu components.
    - Enhanced user experience with responsive design and interactive elements.

--- client/src/components/dashboard-panel/Navbar.tsx
+++ client/src/components/dashboard-panel/Navbar.tsx
@@ -1,11 +1,12 @@
 "use client"
+import Link from "next/link"
 import RightMenu from "./RightMenu"
 
 export default function Navbar() {
   /**
    * ! STATE (état, données) de l'application
    */
-
+  const navigationLinks = [{ href: "/dashboard", label: "Dashboard" }]
   /**
    * ! COMPORTEMENT (méthodes, fonctions) de l'application
    */
@@ -17,13 +18,23 @@ export default function Navbar() {
     <header className="border-b px-4 md:px-14">
       <div className="flex h-16 items-center justify-between gap-4">
         {/* Left side */}
-        <div className="flex items-center gap-6">
-          {/* <Link href="#" className="text-primary hover:text-primary/90 font-semibold ">
-            Hono
-          </Link> */}
-
-          {/* <NavigationMenuDesktop /> */}
-        </div>
+        <nav className="flex items-center gap-8">
+          <Link
+            href="#"
+            className="text-xl font-bold  font-mansalva"
+          >
+            Hono-API
+          </Link>
+          <ul className="hidden md:flex items-center gap-6 text-sm font-medium text-muted-foreground">
+            {navigationLinks.map((link) => (
+              <li key={link.href}>
+                <Link href={link.href} className="hover:text-primary">
+                  {link.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
 
         {/* Right side */}
         <RightMenu />
