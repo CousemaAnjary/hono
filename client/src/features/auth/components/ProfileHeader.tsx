69a16bcdb3ba6c683466c4f948ba829df3ba7915
CousemaAnjary <anjaryabdillah@gmail.com>
2025-06-26 17:01:47 +0300

    feat: mettre à jour le composant ProfileHeader pour utiliser Avatar et afficher le nom de l'utilisateur

--- client/src/features/auth/components/ProfileHeader.tsx
+++ client/src/features/auth/components/ProfileHeader.tsx
@@ -1,23 +1,23 @@
 "use client"
-import Image from "next/image"
 import profileCover from "@/public/images/profile-cover.jpg"
+import { Avatar, AvatarFallback, AvatarImage } from "@/src/components/ui/avatar"
+import Image from "next/image"
+import { useCurrentUser } from "../queries/useCurrentUser"
 
 export default function ProfileHeader() {
   /**
    * ! STATE (état, données) de l'application
    */
-  
-  
+  const { data: userPayload } = useCurrentUser()
   /**
    * ! COMPORTEMENT (méthodes, fonctions) de l'application
    */
-  
-  
+
   /**
    * ! AFFICHAGE (render) de l'application
    */
   return (
-  <div className="mx-auto overflow-hidden rounded-md bg-white shadow-sm">
+    <div className="mx-auto overflow-hidden rounded-md bg-white shadow-sm">
       <div className="relative h-72 w-full">
         <Image
           src={profileCover}
@@ -28,22 +28,18 @@ export default function ProfileHeader() {
       </div>
 
       <div className="relative -mt-20 flex flex-col items-start px-6 pb-6">
-        <div className="relative size-36 overflow-hidden rounded-xl ">
-          
-          {/* <img
-            src={
-              session?.user.image ||
-              `https://api.dicebear.com/9.x/lorelei/svg?seed=${session?.user.email}`
-            }
-            alt="Profile picture"
-            width={144}
-            height={144}
-            className="size-full object-cover"
-          /> */}
+        <div className="relative">
+          <Avatar className="h-36 w-36 rounded-xl shadow-md">
+            <AvatarImage
+              src="https://github.com/evilrabbit.png"
+              alt="Profile image"
+            />
+            <AvatarFallback>AC</AvatarFallback>
+          </Avatar>
         </div>
 
         <h2 className="mt-2 font-inter font-semibold text-gray-800">
-          {/* {session?.user.name} */}
+          {userPayload?.name}
         </h2>
         <p className="mt-1 text-center font-spaceGrotesk text-sm text-muted-foreground max-md:mt-2 max-md:text-left">
           Explorateur curieux, toujours en quête de nouvelles expériences
