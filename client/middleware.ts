01792dda8a95e03f36666c22deae4cbc4d85df39
CousemaAnjary <anjaryabdillah@gmail.com>
2025-06-27 18:35:06 +0300

    feat: ajouter des routes protégées supplémentaires dans le middleware

--- client/middleware.ts
+++ client/middleware.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server"
 
 
 
-const protectedRoutes = ["/dashboard"]
+const protectedRoutes = ["/dashboard", "/profile", "/mangas", "/planning"]
 const publicRoutes = ["/", "/login", "/register"]
 
 export async function middleware(request: NextRequest) {
