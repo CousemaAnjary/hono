612ceadb1a5d4de3c9fd1626302a8968fbc48d39
CousemaAnjary <anjaryabdillah@gmail.com>
2025-06-28 18:59:16 +0300

    fix: ajouter un commentaire pour clarifier le carrousel des mangas en vedette

--- client/app/(protected)/mangas/page.tsx
+++ client/app/(protected)/mangas/page.tsx
@@ -14,6 +14,7 @@ export default function MangasPage() {
    */
   return (
     <>
+      {/* Carrousel des mangas en vedette */}
       <FeatureMangas />
       {/* Filtres */}
       {/* <div className="mb-6">
