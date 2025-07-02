a32c782ab5e3e20584d33faf71db9be83f48204b
CousemaAnjary <anjaryabdillah@gmail.com>
2025-07-02 19:55:06 +0300

    fix: supprimer des lignes vides inutiles dans useLogin et useLogout

--- client/src/features/auth/hooks/useLogin.ts
+++ client/src/features/auth/hooks/useLogin.ts
@@ -1,6 +1,7 @@
 import { login } from "../services/auth.service"
 import { useMutation } from "@tanstack/react-query"
 
+
 export const useLogin = () => {
   return useMutation({
     mutationFn: login,
--- client/src/features/auth/hooks/useLogout.ts
+++ client/src/features/auth/hooks/useLogout.ts
@@ -2,6 +2,7 @@ import { queryClient } from "@/src/lib/react-query"
 import { useMutation } from "@tanstack/react-query"
 import { logout } from "../services/auth.service"
 
+
 export const useLogout = () => {
   return useMutation({
     mutationFn: logout,
