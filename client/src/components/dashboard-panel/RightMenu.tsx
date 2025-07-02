45a17c3bb77c9135021df3e5b4d51ec7eccb9115
CousemaAnjary <anjaryabdillah@gmail.com>
2025-07-02 23:15:50 +0300

    feat: ajouter le composant SearchButton avec état, comportement et affichage

--- client/src/components/dashboard-panel/RightMenu.tsx
+++ client/src/components/dashboard-panel/RightMenu.tsx
@@ -3,6 +3,7 @@
 import { Search } from "lucide-react"
 import UserMenu from "../../features/user/components/UserMenu"
 import NotificationMenu from "./NotificationMenu"
+import SearchButton from "./SearchButton"
 
 export default function RightMenu() {
   /**
@@ -19,18 +20,7 @@ export default function RightMenu() {
   return (
     <div className="flex items-center gap-4">
       {/* Barre de recherche stylisée */}
-      <div className="relative hidden md:flex items-center">
-        <button
-          type="button"
-          className="group flex items-center gap-2 rounded-md border px-3 py-1.5 text-sm text-muted-foreground shadow-sm transition hover:bg-muted"
-        >
-          <Search className="h-4 w-4 text-gray-500" />
-          <span className="text-sm text-gray-500 font-spaceGrotesk">Rechercher tout</span>
-          <kbd className="ml-auto rounded bg-gray-100 px-1.5 py-0.5 text-xs text-gray-600 font-spaceGrotesk">
-            CTRL + K
-          </kbd>
-        </button>
-      </div>
+       <SearchButton />
 
       <div className="flex items-center gap-2">
         <NotificationMenu />
---
+++ client/src/components/dashboard-panel/SearchButton.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import { Search } from "lucide-react"
+
+export default function SearchButton() {
+  /**
+   * ! STATE (état, données) de l'application
+   */
+  
+  
+  /**
+   * ! COMPORTEMENT (méthodes, fonctions) de l'application
+   */
+  
+  
+  /**
+   * ! AFFICHAGE (render) de l'application
+   */
+  return (
+     <div className="relative hidden md:flex items-center">
+      <button
+        type="button"
+        className="group flex items-center gap-2 rounded-md border px-3 py-1.5 text-sm text-muted-foreground shadow-sm transition hover:bg-muted"
+      >
+        <Search className="h-4 w-4 text-gray-500" />
+        <span className="text-sm text-gray-500 font-spaceGrotesk">Rechercher tout</span>
+        <kbd className="ml-auto rounded bg-gray-100 px-1.5 py-0.5 text-xs text-gray-600 font-spaceGrotesk">
+          CTRL + K
+        </kbd>
+      </button>
+    </div>
+  )
+}
