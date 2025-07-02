fccdb85324985f319e402996f0130bf01a6999ec
CousemaAnjary <anjaryabdillah@gmail.com>
2025-07-02 23:50:51 +0300

    fix: supprimer les importations inutilisées dans le composant RightMenu

--- client/src/components/dashboard-panel/SearchButton.tsx
+++ client/src/components/dashboard-panel/SearchButton.tsx
@@ -2,7 +2,11 @@
 
 import { Search } from "lucide-react"
 
-export default function SearchButton() {
+interface SearchButtonProps {
+  onClick: () => void
+}
+
+export default function SearchButton({ onClick }: SearchButtonProps) {
   /**
    * ! STATE (état, données) de l'application
    */
@@ -20,11 +24,12 @@ export default function SearchButton() {
      <div className="relative hidden md:flex items-center">
       <button
         type="button"
+         onClick={onClick}
         className="group flex items-center gap-2 rounded-md border px-3 py-1.5 text-sm text-muted-foreground shadow-sm transition hover:bg-muted"
       >
         <Search className="h-4 w-4 text-gray-500" />
         <span className="text-sm text-gray-500 font-spaceGrotesk">Rechercher tout</span>
-        <kbd className="ml-auto rounded bg-gray-100 px-1.5 py-0.5 text-xs text-gray-600 font-spaceGrotesk">
+        <kbd className="ml-auto rounded bg-gray-100 px-1.5 py-0.5 text-xs text-gray-700 font-spaceGrotesk">
           CTRL + K
         </kbd>
       </button>
