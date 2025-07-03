323da0a010296b3c4b121f5a55a31f9340131093
CousemaAnjary <anjaryabdillah@gmail.com>
2025-07-03 18:42:46 +0300

    fix: réorganiser les importations et améliorer la structure du composant AvatarUpload

--- client/src/features/user/components/AvatarUpload.tsx
+++ client/src/features/user/components/AvatarUpload.tsx
@@ -1,30 +1,28 @@
 "use client"
-import { useCurrentUser } from "../queries/useCurrentUser"
 import { Avatar, AvatarImage } from "@/src/components/ui/avatar"
-
+import { useCurrentUser } from "../queries/useCurrentUser"
 
 export default function AvatarUpload() {
   /**
    * ! STATE (état, données) de l'application
    */
-   const { data: userPayload } = useCurrentUser()
-  
+  const { data: userPayload } = useCurrentUser()
+
   /**
    * ! COMPORTEMENT (méthodes, fonctions) de l'application
    */
-  
-  
+
   /**
    * ! AFFICHAGE (render) de l'application
    */
   return (
-    <div className="relative">
-          <Avatar className="h-36 w-36 rounded-xl border-2 border-white shadow-md">
-            <AvatarImage
-              src= {`https://api.dicebear.com/9.x/lorelei/svg?seed=${userPayload?.email}`}
-              alt="Profile image"
-            />
-          </Avatar>
-        </div>
+    <div className="relative inline-flex">
+      <Avatar className="h-36 w-36 rounded-xl border-2 border-white shadow-md">
+        <AvatarImage
+          src={`https://api.dicebear.com/9.x/lorelei/svg?seed=${userPayload?.email}`}
+          alt="Profile image"
+        />
+      </Avatar>
+    </div>
   )
 }
