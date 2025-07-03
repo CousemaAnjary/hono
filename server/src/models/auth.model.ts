705201faad9b106c4d473f7441004682dd914703
CousemaAnjary <anjaryabdillah@gmail.com>
2025-07-03 18:10:08 +0300

    feat: ajouter la colonne 'image' à la table 'users' et mettre à jour les fichiers de migration

--- server/src/models/auth.model.ts
+++ server/src/models/auth.model.ts
@@ -6,6 +6,7 @@ export const users = pgTable("users", {
   name: text("name").notNull(),
   email: text("email").notNull().unique(),
   password: text("password").notNull(),
+  image: text("image"),
   createdAt: timestamp("created_at").defaultNow(),
 })
 
