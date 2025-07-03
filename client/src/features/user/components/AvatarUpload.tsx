a0f780984b6fd6964a0227093567c478d57d599b
CousemaAnjary <anjaryabdillah@gmail.com>
2025-07-03 22:13:12 +0300

    fix: réorganiser les importations et améliorer les classes CSS dans AvatarUpload

--- client/src/features/user/components/AvatarUpload.tsx
+++ client/src/features/user/components/AvatarUpload.tsx
@@ -1,11 +1,10 @@
 /* eslint-disable @next/next/no-img-element */
 "use client"
-import { XIcon } from "lucide-react"
 import user from "@/public/images/user.png"
 import { Button } from "@/src/components/ui/button"
 import { useFileUpload } from "@/src/hooks/use-file-upload"
+import { PencilIcon, XIcon } from "lucide-react"
 import { useCurrentUser } from "../queries/useCurrentUser"
-
 
 export default function AvatarUpload() {
   /**
@@ -13,7 +12,8 @@ export default function AvatarUpload() {
    */
   const { data: userPayload } = useCurrentUser()
   const previewUrl = userPayload?.image || null
-  const [{ files }, { removeFile, openFileDialog, getInputProps }] = useFileUpload({
+  const [{ files }, { removeFile, openFileDialog, getInputProps }] =
+    useFileUpload({
       accept: "image/*",
     })
   /**
@@ -27,7 +27,7 @@ export default function AvatarUpload() {
     <div className="relative">
       <Button
         variant="outline"
-        className="relative size-36 rounded-xl border-2 border-white p-0 shadow-md overflow-hidden bg-transparent "
+        className="relative size-36 rounded-xl border-2 border-white p-0 shadow-md overflow-hidden bg-transparent group"
         onClick={openFileDialog}
         aria-label={previewUrl ? "Changer l'image" : "Uploader une image"}
       >
@@ -35,15 +35,20 @@ export default function AvatarUpload() {
           <img
             src={previewUrl}
             alt="Aperçu de l'image"
-            className="size-full object-cover"
+            className="size-full object-cover transition-all duration-300 group-hover:blur-sm"
           />
         ) : (
           <img
             src={typeof user === "string" ? user : user.src}
             alt="Image de profil par défaut"
-            className="size-24"
+            className="size-24 transition-all duration-300 group-hover:blur-sm"
           />
         )}
+
+        {/* Icône d'édition qui apparaît au hover */}
+        <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-black/20">
+          <PencilIcon className="size-8 text-white drop-shadow-lg" />
+        </div>
       </Button>
 
       {previewUrl && (
