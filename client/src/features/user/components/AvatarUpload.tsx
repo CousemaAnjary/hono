bc899ae08858975dc1f9a2034e7e9b537be354b5
CousemaAnjary <anjaryabdillah@gmail.com>
2025-07-04 19:05:13 +0300

    fix: simplifier le composant AvatarUpload en supprimant le code inutilisé et en réorganisant les importations

--- client/src/features/user/components/AvatarUpload.tsx
+++ client/src/features/user/components/AvatarUpload.tsx
@@ -1,28 +1,20 @@
 /* eslint-disable @next/next/no-img-element */
 "use client"
 import user from "@/public/images/user.png"
+import { Pencil, XIcon } from "lucide-react"
 import { Button } from "@/src/components/ui/button"
 import { useFileUpload } from "@/src/hooks/use-file-upload"
-import { Pencil, XIcon } from "lucide-react"
-import { useState } from "react"
-import AvatarCropDialog from "./AvatarCropDialog"
+
 
 export default function AvatarUpload() {
   /**
    * ! STATE (état, données) de l'application
    */
   // const { data: userPayload } = useCurrentUser()
-  const [{ files }, { removeFile, openFileDialog, getInputProps }] =
-    useFileUpload({
+  const [{ files }, { removeFile, openFileDialog, getInputProps }] = useFileUpload({
       accept: "image/*",
     })
   const previewUrl = files[0]?.preview || null
-  const fileId = files[0]?.id
-  const [finalImageUrl, setFinalImageUrl] = useState<string | null>(null)
-  const [isDialogOpen, setIsDialogOpen] = useState(false)
-
-  const imageSrc = finalImageUrl || (typeof user === "string" ? user : user.src)
-
   /**
    * ! COMPORTEMENT (méthodes, fonctions) de l'application
    */
@@ -39,11 +31,9 @@ export default function AvatarUpload() {
       >
         <div className="absolute inset-0">
           <img
-            src={imageSrc}
+            src={previewUrl || user.src}
             alt="Image de profil"
-            className={`h-full w-full object-cover transition-all duration-200 group-hover:blur-sm ${
-              !previewUrl ? "object-contain p-5" : ""
-            }`}
+            className={`h-full w-full object-cover transition-all duration-200 group-hover:blur-sm ${!previewUrl ? "object-contain p-5" : ""}`}
           />
         </div>
 
@@ -70,14 +60,7 @@ export default function AvatarUpload() {
         aria-label="Uploader un fichier image"
         tabIndex={-1}
       />
-
-      <AvatarCropDialog
-        isOpen={isDialogOpen}
-        setIsOpen={setIsDialogOpen}
-        previewUrl={previewUrl}
-        fileId={fileId}
-        setFinalImageUrl={setFinalImageUrl}
-      />
+      
     </div>
   )
 }
