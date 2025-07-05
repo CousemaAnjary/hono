57167538433871c96a4a3d6ba9081984089bf869
CousemaAnjary <anjaryabdillah@gmail.com>
2025-07-05 21:28:20 +0300

    feat: remplacer la fonction de connexion par une mutation utilisant authFetch

--- client/src/features/auth/hooks/useLogin.ts
+++ client/src/features/auth/hooks/useLogin.ts
@@ -1,9 +1,18 @@
-import { login } from "../services/auth.service"
+import { z } from "zod"
+import { LoginResponse } from "../types/auth"
+import { authFetch } from "@/src/lib/authFetch"
 import { useMutation } from "@tanstack/react-query"
+import { loginSchema } from "@/src/validators/auth.validator"
 
 
 export const useLogin = () => {
   return useMutation({
-    mutationFn: login,
+
+    mutationFn: async (data: z.infer<typeof loginSchema>) => {
+      return await authFetch<LoginResponse>("/auth/login", {
+          method: "POST",
+          body: JSON.stringify(data),
+        })
+    }
   })
 }
